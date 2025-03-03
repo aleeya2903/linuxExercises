02955624bd2d3eb87279d17cf81d1cec7cd0ecfb
youngwoo0425 <ykim966@wisc.edu>
2025-03-03 15:17:07 -0600

    Calculated total and average value

--- school.sh
+++ school.sh
@@ -10,11 +10,9 @@ cut -d',' -f7 | \
         total=$((total + value))
         count=$((count + 1))
     done
-    if [ $count -gt 0 ]; then
-        # Calculate and display the average
-        average=$(echo "$total / $count" | bc -l)
-	echo "Total value for MADISON SCHOOLS: $total"
-	echo "Average value for MADISON SCHOOLS: $average"
-    fi
+    
+    average=$(echo "$total / $count" | bc -l)
+    echo "Total value for MADISON SCHOOLS: $total"
+    echo "Average value for MADISON SCHOOLS: $average"
     
 }
