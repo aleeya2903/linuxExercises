7744c8e1b7048e878ca2be241f021fb8c5bdb698
youngwoo0425 <ykim966@wisc.edu>
2025-03-03 15:13:23 -0600

    Calculated total and average value

--- school.sh
+++ school.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+cat Property_Tax_Roll.csv | \
+grep "MADISON SCHOOLS" | \
+cut -d',' -f7 | \
+{
+    total=0
+    count=0
+    while read value; do
+        total=$((total + value))
+        count=$((count + 1))
+    done
+    if [ $count -gt 0 ]; then
+        # Calculate and display the average
+        average=$(echo "$total / $count" | bc -l)
+	echo "Total value for MADISON SCHOOLS: $total"
+	echo "Average value for MADISON SCHOOLS: $average"
+    fi
+    
+}
